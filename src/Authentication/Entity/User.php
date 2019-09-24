135a8c883c141ff4811fcf646d4d07c7f3e3d1c7
Andreas Möller <am@localheinz.com>
2019-09-24 10:37:13 +0200

    Fix: Revert additions to user entity

--- src/Authentication/Entity/User.php
+++ src/Authentication/Entity/User.php
@@ -6,23 +6,4 @@ namespace Authentication\Entity;
 
 final class User
 {
-    private $email;
-
-    private $password;
-
-    public function __construct(string $email, string $password)
-    {
-        $this->email = $email;
-        $this->password = $password;
-    }
-
-    public function email(): string
-    {
-        return $this->email;
-    }
-
-    public function password(): string
-    {
-        return $this->password;
-    }
 }
