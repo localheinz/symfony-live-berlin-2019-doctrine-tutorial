d6d96ad5caea499a7808f157cf3b4604ee28af76
Andreas Möller <am@localheinz.com>
2019-09-24 12:47:16 +0200

    Fix: Pull up method

--- src/Application/Container.php
+++ src/Application/Container.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Application;
 
 use Domain\Authentication\Repository\Users;
+use Domain\Authentication\Service\PasswordEncoder;
 
 interface Container
 {
     public function users(): Users;
+
+    public function passwordEncoder(): PasswordEncoder;
 }
