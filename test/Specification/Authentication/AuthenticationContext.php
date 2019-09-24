5b1ed7d0960a73aae276945cdc977a8eeb22aa18
Andreas Möller <am@localheinz.com>
2019-09-24 10:05:37 +0200

    Enhancement: Define missing steps

--- test/Specification/Authentication/AuthenticationContext.php
+++ test/Specification/Authentication/AuthenticationContext.php
@@ -5,7 +5,47 @@ declare(strict_types=1);
 namespace Test\Specification\Authentication;
 
 use Behat\Behat\Context\Context;
+use Behat\Behat\Tester\Exception\PendingException;
 
 final class AuthenticationContext implements Context
 {
+    /**
+     * @Given there are no registered users
+     */
+    public function thereAreNoRegisteredUsers()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When a user registers with the website
+     */
+    public function aUserRegistersWithTheWebsite()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the user can log into the website
+     */
+    public function theUserCanLogIntoTheWebsite()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given there is a registered user
+     */
+    public function thereIsARegisteredUser()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the user cannot log into the website with a non-matching password
+     */
+    public function theUserCannotLogIntoTheWebsiteWithANonMatchingPassword()
+    {
+        throw new PendingException();
+    }
 }
--- test/Specification/Blog/BlogPostContext.php
+++ test/Specification/Blog/BlogPostContext.php
@@ -5,7 +5,111 @@ declare(strict_types=1);
 namespace Test\Specification\Blog;
 
 use Behat\Behat\Context\Context;
+use Behat\Behat\Tester\Exception\PendingException;
 
 final class BlogPostContext implements Context
 {
+    /**
+     * @Given an authenticated user
+     */
+    public function anAuthenticatedUser()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When the user creates a new blog post
+     */
+    public function theUserCreatesANewBlogPost()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the blog post can be viewed
+     */
+    public function theBlogPostCanBeViewed()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the blog post author corresponds to the user that created it
+     */
+    public function theBlogPostAuthorCorrespondsToTheUserThatCreatedIt()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given an authenticated user and an existing blog post by another user
+     */
+    public function anAuthenticatedUserAndAnExistingBlogPostByAnotherUser()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When the user comments on the blog post
+     */
+    public function theUserCommentsOnTheBlogPost()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the blog post comment can be viewed
+     */
+    public function theBlogPostCommentCanBeViewed()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given an authenticated user and an existing blog post by the same user
+     */
+    public function anAuthenticatedUserAndAnExistingBlogPostByTheSameUser()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the user cannot comment on the blog post
+     */
+    public function theUserCannotCommentOnTheBlogPost()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given an existing set of blog posts with comments
+     */
+    public function anExistingSetOfBlogPostsWithComments()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I view the list of commented blog posts
+     */
+    public function iViewTheListOfCommentedBlogPosts()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the list of popular commented  blog posts should be sorted by comment count
+     */
+    public function theListOfPopularCommentedBlogPostsShouldBeSortedByCommentCount()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the list of popular commented blog posts should be paginated
+     */
+    public function theListOfPopularCommentedBlogPostsShouldBePaginated()
+    {
+        throw new PendingException();
+    }
 }
